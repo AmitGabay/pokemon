a29710767a4b17402a18fe69c76878bd1e5cee1a
Amit <amit.b.gabay@gmail.com>
2022-02-13 14:24:05 +0200

    add css modules to Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,14 @@
-import "./Card.css";
+import style from "./Card.module.css";
 
 const Card = (props) => {
   return (
-    <div className="card">
-      <div className="card-header">
-        <h2 className="card-name">Pokemon Name</h2>
-        <span className="card-icon">icon</span>
+    <div className={style.card}>
+      <div className={style.header}>
+        <h2 className={style.name}>Pokemon Name</h2>
+        <span className={style.icon}>icon</span>
       </div>
       <img
-        className="card-img"
+        className={style.img}
         src="https://dalicanvas.co.il/wp-content/uploads/2020/02/%D7%A9%D7%A7%D7%99%D7%A2%D7%94-%D7%A7%D7%9C%D7%90%D7%A1%D7%99%D7%AA-1.jpg"
         alt="pic"
       />
