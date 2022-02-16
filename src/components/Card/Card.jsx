25c3496a3008b72c4b8c765002b45b1eb7567346
Amit <amit.b.gabay@gmail.com>
2022-02-16 18:37:30 +0200

    add get request fot type and image

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,19 +2,30 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import style from "./Card.module.css";
 
-const Card = (props) => {
-  function randomNum() {
-    return Math.floor(Math.random() * 20) + 1;
-  }
+function randomNum() {
+  return Math.floor(Math.random() * 898) + 1;
+}
 
+const Card = (props) => {
+  // const [{ pokemonName, pokemonType, pokemonImg }, setPokemon] = useState();
   const [pokemonName, setName] = useState("");
+  const [pokemonType, setType] = useState([]);
+  const [pokemonImg, setImg] = useState("");
 
   useEffect(() => {
     axios({
       method: "get",
       url: `https://pokeapi.co/api/v2/pokemon/${randomNum()}`,
     }).then((res) => {
+      //   setPokemon({
+      //     res.data.species.name,
+      //     [...res.data.types.map((type) => type.name)],
+      //     res.data.forms[0].url.sprites.front_default,
+      //   });
+      // });
       setName(res.data.species.name);
+      setType([res.data.types.map((type) => type.type.name)]);
+      setImg(res.data.forms[0].url.sprites.front_default);
     });
   }, []);
 
@@ -22,13 +33,11 @@ const Card = (props) => {
     <div className={style.card}>
       <div className={style.header}>
         <h2 className={style.name}>{pokemonName}</h2>
-        <span className={style.icon}>icon</span>
+        {pokemonType.map((type) => (
+          <span className={style.icon}>{type}</span>
+        ))}
       </div>
-      <img
-        className={style.img}
-        src="https://dalicanvas.co.il/wp-content/uploads/2020/02/%D7%A9%D7%A7%D7%99%D7%A2%D7%94-%D7%A7%D7%9C%D7%90%D7%A1%D7%99%D7%AA-1.jpg"
-        alt="pic"
-      />
+      {/* <img className={style.img} src={pokemonImg} alt="pic" /> */}
     </div>
   );
 };
