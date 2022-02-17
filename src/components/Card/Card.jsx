1144e724a6d575b10bcb2f767ff584d1435f96ab
Amit <amit.b.gabay@gmail.com>
2022-02-17 13:24:25 +0200

    complite get request for card and extra styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,25 +7,20 @@ function randomNum() {
 }
 
 const Card = (props) => {
-  // const [{ pokemonName, pokemonType, pokemonImg }, setPokemon] = useState();
   const [pokemonName, setName] = useState("");
   const [pokemonType, setType] = useState([]);
   const [pokemonImg, setImg] = useState("");
+  const [pokemonAbility, setAbility] = useState([]);
 
   useEffect(() => {
     axios({
       method: "get",
       url: `https://pokeapi.co/api/v2/pokemon/${randomNum()}`,
     }).then((res) => {
-      //   setPokemon({
-      //     res.data.species.name,
-      //     [...res.data.types.map((type) => type.name)],
-      //     res.data.forms[0].url.sprites.front_default,
-      //   });
-      // });
       setName(res.data.species.name);
-      setType([res.data.types.map((type) => type.type.name)]);
-      setImg(res.data.forms[0].url.sprites.front_default);
+      setType(res.data.types.map((type) => type.type.name));
+      setImg(res.data.sprites.front_default);
+      setAbility(res.data.abilities.map((ability) => ability.ability.name));
     });
   }, []);
 
@@ -33,11 +28,19 @@ const Card = (props) => {
     <div className={style.card}>
       <div className={style.header}>
         <h2 className={style.name}>{pokemonName}</h2>
-        {pokemonType.map((type) => (
-          <span className={style.icon}>{type}</span>
+        <div className={style.icon}>
+          {pokemonType.map((type) => (
+            <span>{type}</span>
+          ))}
+        </div>
+      </div>
+      <img className={style.img} src={pokemonImg} alt="pic" />
+      <div className={style.ability}>
+        <h4 className={style.abilities}>Abilities:</h4>
+        {pokemonAbility.map((ability) => (
+          <span>{ability}</span>
         ))}
       </div>
-      {/* <img className={style.img} src={pokemonImg} alt="pic" /> */}
     </div>
   );
 };
